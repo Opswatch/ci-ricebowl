c5ed316fe2e56b1e85b9bbc45e99ca0a6a6aa60a
Hugo Pinheiro <hpinheiro@echidna.ca>
2019-01-03 14:35:02 -0500

    fix wp-auto-update.sh to run drupal instead of wordpress

--- bin/wp-auto-update.sh
+++ bin/wp-auto-update.sh
@@ -14,7 +14,7 @@ php -f bin/slack_notify.php pantheon_multidev_setup
 echo -e "\nDeleting the ${TERMINUS_ENV} multidev environment..."
 terminus multidev:delete $SITE_UUID.$TERMINUS_ENV --delete-branch --yes
 echo -e "\nRe-creating the ${TERMINUS_ENV} multidev environment..."
-terminus multidev:create $SITE_UUID.live $TERMINUS_ENV
+terminus multidev:create $SITE_UUID.test $TERMINUS_ENV
 
 # check for upstream updates
 echo -e "\nChecking for upstream updates on the ${TERMINUS_ENV} multidev..."
@@ -46,11 +46,12 @@ terminus connection:set $SITE_UUID.$TERMINUS_ENV sftp
 # waking the site
 terminus env:wake -n $SITE_UUID.$TERMINUS_ENV
 
-# check for Drupal plugin updates
-echo -e "\nChecking for Drupal plugin updates on the ${TERMINUS_ENV} multidev..."
-PLUGIN_UPDATES="$(terminus wp $SITE_UUID.$TERMINUS_ENV -- plugin update --dry-run --all --format=summary)"
+# check for Drupal module updates
+echo -e "\nChecking for Drupal module updates on the ${TERMINUS_ENV} multidev..."
+terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus
+PLUGIN_UPDATES="$(terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatestatus --format=list | grep -v ok)"
 
-if [[ ${PLUGIN_UPDATES} == "No plugin updates available." ]]
+if [[ ${PLUGIN_UPDATES} == "No module updates available." ]]
 then
     # no Drupal module updates found
     echo -e "\nNo Drupal module updates found on the ${TERMINUS_ENV} multidev..."
@@ -59,8 +60,8 @@ else
     # update Drupal modules
     echo -e "\nUpdating Drupal modules on the ${TERMINUS_ENV} multidev..."
     php -f bin/slack_notify.php drupal_moduleupdates ${PLUGIN_UPDATES}
-    php -f bin/slack_notify.php terminus_pluginupdates
-    terminus wp $SITE_UUID.$TERMINUS_ENV -- plugin update --all
+    php -f bin/slack_notify.php terminus_moduleupdates
+    terminus drush $SITE_UUID.$TERMINUS_ENV -- pm-updatecode --no-core --yes
 
     # wake the site environment before committing code
     echo -e "\nWaking the ${TERMINUS_ENV} multidev..."
@@ -113,10 +114,10 @@ else
         php -f bin/slack_notify.php pantheon_deploy dev
         terminus multidev:merge-to-dev $SITE_UUID.$TERMINUS_ENV
 
-        # # deploy to test
-        # echo -e "\nDeploying the updates from dev to test..."
-        # # php -f bin/slack_notify.php pantheon_deploy test
-        # terminus env:deploy $SITE_UUID.test --sync-content --cc --note="Auto deploy of Wordpress updates (core, plugins)" --updatedb
+        # deploy to test
+        echo -e "\nDeploying the updates from dev to test..."
+        php -f bin/slack_notify.php pantheon_deploy test
+        terminus env:deploy $SITE_UUID.test --sync-content --cc --note="Auto deploy of Drupal updates (core, plugins)" --updatedb
 
         # # backup the live site
         # echo -e "\nBacking up the live environment..."
@@ -126,7 +127,7 @@ else
         # # deploy to live
         # echo -e "\nDeploying the updates from test to live..."
         # # php -f bin/slack_notify.php pantheon_deploy live
-        # terminus env:deploy $SITE_UUID.live --cc --note="Auto deploy of Drupal updates (core, plugins)" --updatedb
+        # terminus env:deploy $SITE_UUID.live --cc --note="Auto deploy of Drupal updates (core, modules)" --updatedb
 
         # echo -e "\nVisual regression tests passed! Drupal updates deployed to live..."
         # # php -f bin/slack_notify.php wizard_done `find . | grep document_0_desktop | grep test`
